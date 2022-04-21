9c361ab274991c211350752f96b83068ed2390e3
s0phyy <103076206+s0phyy@users.noreply.github.com>
2022-04-21 17:19:01 -0300

    projeto 11 atualizado

--- sketch.js
+++ sketch.js
@@ -1,25 +1,72 @@
-var ship,sea;
-var shipImg, seaImg;
+var garden,rabbit,apple, redleaves,orangeleaves;
+var appleImg,gardenImg,rabbitImg,orangeImg,redImg;
+var aleatorio;
+
 function preload(){
-shipImg = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
-seaImg = loadImage("sea.png");
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+ orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png"); 
+
 }
 
 function setup(){
+  
   createCanvas(400,400);
-  sea = createSprite(200, 200, 400, 400)
-  sea.addImage("mar", seaImg);
-  ship = createSprite(130, 270, 10, 10);
-  ship.addAnimation("navio", shipImg)
-  ship.scale = 0.3;
+  
+// mover o fundo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
 
-  sea.x = sea.width/2;
-  sea.velocityX = -2;
+//criar sprite do coelho
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
 }
 
-function draw() {
-  background("blue");
-    drawSprites();
 
- 
+function draw() {
+  background(0);
+
+  aleatorio =  Math.round(random(20, 370));
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  rabbit.x = mouseX;
+
+   maçã();
+   folhas_laranjas();
+   folhas_vermelhas();
+  drawSprites();
+  
+}
+
+
+function maçã(){
+  if(frameCount%89===0){
+    apple = createSprite(aleatorio,-470,10,10);
+    apple.velocityY = 5;
+    apple.addImage("maçã", appleImg);
+    apple.scale = 0.07;
+  }
+}
+
+function folhas_laranjas(){
+  if(frameCount%67===0){
+    orangeleaves = createSprite(aleatorio,-430,10,10);
+    orangeleaves.velocityY = 5;
+    orangeleaves.addImage("folha laranja", orangeImg);
+    orangeleaves.scale = 0.07;
+  }
+}
+
+function folhas_vermelhas(){
+  if(frameCount%103===0){
+    redleaves = createSprite(aleatorio,-410,10,10);
+    redleaves.velocityY = 5;
+    redleaves.addImage("folha vermelha", redImg);
+    redleaves.scale = 0.07;
+  }
 }
